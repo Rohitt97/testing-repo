cadc32c722ae703267166645bb6f8be248ec806c
Rohitt97 <rohittkumawat97@gmail.com>
2024-01-29 10:57:38 +0530

    First Commit

--- index.js
+++ index.js
@@ -1,8 +1,48 @@
+// const express = require('express');
+// const Pusher = require('pusher');
+// const cors = require('cors');
+
+// const app = express();
+
+// const pusher = new Pusher({
+//     appId: "1743351",
+//     key: "601475aebc168874cce3",
+//     secret: "c11e1aac2ca0ba844095",
+//     cluster: "ap2",
+//     encrypted: true
+// });
+
+// //MiddleWare to parse the JSON Requests.
+// app.use(express.json());
+// app.use(cors());
+
+// app.post('/test-notification', (req, res) => {
+//     const { message } = req.body;
+//     console.log(req.body);
+
+//     if (!message) {
+//         res.status(400).json({ error: "Message is required" });
+//     }
+
+//     pusher.trigger('test-channel', 'test-notification', {
+//         message,
+//     });
+
+//     //Responding the success message.
+//     res.send({ success: true, message: "Notification sent Successfully." });
+
+// });
+
+// app.listen(4000);
+
 const express = require('express');
+const bodyParser = require('body-parser');
 const Pusher = require('pusher');
-const cors = require('cors');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const pusher = new Pusher({
     appId: "1743351",
@@ -12,25 +52,16 @@ const pusher = new Pusher({
     encrypted: true
 });
 
-//MiddleWare to parse the JSON Requests.
-app.use(express.json());
-app.use(cors());
-
-app.post('/test-notification', (req, res) => {
+app.post('/message', (req, res) => {
     const { message } = req.body;
-    console.log(req.body);
 
-    if (!message) {
-        res.status(400).json({ error: "Message is required" });
-    }
+    // Trigger the 'new-message' event in the 'chat' channel
+    pusher.trigger('chat', 'new-message', { message });
 
-    pusher.trigger('test-channel', 'test-notification', {
-        message,
-    });
-
-    //Responding the success message.
-    res.send({ success: true, message: "Notification sent Successfully." });
-
+    res.status(200).send('Message sent');
 });
 
-app.listen(4000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
